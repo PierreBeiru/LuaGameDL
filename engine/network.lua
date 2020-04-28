f10402a4424ee3896dbf0fa643f199f0750625e6
Jon Vail <jon.vail@yahoo.com>
2020-04-28 19:22:07 -0400

    Create network.lua

---
+++ engine/network.lua
@@ -0,0 +1,23 @@
+--[[
+
+]]
+
+local network = {}
+
+do
+  local socket = require "socket"
+  udp = socket.udp()
+  udp:setsockname('*',77777)
+  udp:settimeout(0)
+end
+
+function network:init()
+  network_udp = udp
+end
+
+function network:update(dt)
+
+end
+
+
+return network
