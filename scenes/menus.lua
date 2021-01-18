210b66e28a885c1831ba0d12c856d2c19c418cc6
Jon Vail <jon.vail@yahoo.com>
2021-01-18 01:01:20 -0500

    Update menus.lua

--- scenes/menus.lua
+++ scenes/menus.lua
@@ -3,6 +3,13 @@
 ]]
 local menus = {}
 
+local current_state = "main"
+--[[
+"main"
+"settings"
+"wiki"
+]]
+
 local main_menu_btns = {}
 main_menu_btns[1] = {x,y,z,width,height,radius,diameter,text=""}
 main_menu_btns[2] = {x,y,z,width,height,radius,diameter,text=""}
@@ -25,7 +32,13 @@ function menus.update(dt)
 
 end
 function menus.draw()
+  if current_state == "main" then
 
+  elseif current_state == "settings" then
+
+  elseif current_state == "wiki" then
+
+  end
 end
 
 function menus.keypressed(key)
@@ -44,60 +57,85 @@ function menus.keypressed(key)
   end
 end
 function menus.isDown(key)
+
 end
 function menus.hasKeyRepeat()
+
 end
 function menus.hasTextInput()
+
 end
 function menus.setKeyRepeat(enable)
+
 end
 function menus.isScancodeDown(scancode, ...)
+
 end
 function menus.getKeyFromScancode(scancode)
+
 end
 function menus.getScancodeFromKey(key)
+
 end
 
 function menus.gamepadaxis(joystick, axis)
+
 end
 function menus.gamepadpressed(joystick, button)
+
 end
 function menus.gamepadreleased(joystick, button)
+
 end
 
 function menus.joystickhat(joystick, hat, direction)
+
 end
 function menus.joystickaxis(joystick, axis, value)
+
 end
 function menus.joystickadded(joystick)
+
 end
 function menus.joystickpressed(joystick, button)
+
 end
 function menus.joystickremoved(joystick)
+
 end
 function menus.joystickreleased(joystick, button)
+
 end
 
 function menus.mousemoved(x, y, dx, dy)
+
 end
 function menus.mousefocus(focus)
+
 end
 function menus.mousepressed(x, y, button, isTouch)
+
 end
 function menus.mousereleased(x, y, button, isTouch)
+
 end
 
 function menus.focus(focus)
+
 end
 
 function menus.wheelmoved(x, y)
+
 end
 
 function menus.touchpressed(id, x, y, dx, dy, pressure)
+
 end
 function menus.touchmoved(id, x, y, dx, dy, pressure)
+
 end
 function menus.touchreleased(id, x, y, dx, dy, pressure)
+
 end
 
 return menus
