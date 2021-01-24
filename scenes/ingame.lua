d0faa1c56087408f11980b087bfc3f2fec1f9e2e
Jon Vail <jon.vail@yahoo.com>
2021-01-24 13:53:36 -0500

    Update ingame.lua
    Minor Changes.

---
+++ scenes/ingame.lua
@@ -0,0 +1,51 @@
+local ingame = {}
+
+function ingame.update(dt)
+
+end
+
+function ingame.draw()
+
+end
+
+function ingame.mousepressed()
+
+end
+
+function ingame.mousereleased()
+
+end
+
+--[[
+function ingame.load() end
+function ingame.draw() end
+function ingame.update() end
+function ingame.keypressed() end
+function ingame.isDown() end
+function ingame.hasKeyRepeat() end
+function ingame.hasTextInput() end
+function ingame.setKeyRepeat() end
+function ingame.isScancodeDown() end
+function ingame.getKeyFromScancode() end
+function ingame.getScancodeFromKey() end
+function ingame.gamepadaxis() end
+scene.gamepadpressed = menus.gamepadpressed
+scene.gamepadreleased = menus.gamepadreleased
+scene.joystickhat = menus.joystickhat
+scene.joystickaxis = menus.joystickaxis
+scene.joystickadded = menus.joystickadded
+scene.joystickpressed = menus.joystickpressed
+scene.joystickremoved = menus.joystickremoved
+scene.joystickreleased = menus.joystickreleased
+scene.mousemoved = menus.mousemoved
+scene.mousefocus = menus.mousefocus
+scene.mousepressed = menus.mousepressed
+scene.mousereleased = menus.mousereleased
+scene.focus = menus.focus
+scene.wheelmoved = menus.wheelmoved
+scene.touchpressed = menus.touchpressed
+scene.touchmoved = menus.touchmoved
+scene.touchreleased = menus.touchreleased
+]]
+
+return ingame 
