9071e5565b5d8a4bcb19a0db1549c2df1027c382
Jon Vail <jon.vail@yahoo.com>
2021-01-26 00:53:04 -0500

    Update settings.lua
    N1

---
+++ scenes/settings.lua
@@ -0,0 +1,22 @@
+local settings = {}
+
+local buttons_tbl = {}
+buttons_tbl[1] = {
+  x = nil,
+  y = nil,
+  z = nil,
+  width = nil,
+  height = nil,
+  function action()
+
+  end,
+  
+}
+
+function settings.draw()
+
+end
+function settings.update(dt)
+
+end
+return settings
