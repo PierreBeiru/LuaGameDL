99f161f58e4112c093d146cc81644f99a26cc595
Jon Vail <jon.vail@yahoo.com>
2021-01-26 01:10:39 -0500

    Update world.lua
    N2

--- data/world.lua
+++ data/world.lua
@@ -4,43 +4,70 @@ local tile_count = 25
 local tile_size_x, tile_size_y = 100, 100 --units
 local tile_size_z = 0
 
-
+--shortform
+local tsx = tile_size_x
+local tsy = tile_size_y
+local tsz = tile_size_z
 
 local tiles = {}
-tiles[1] = {}
-tiles[2] = {}
-tiles[3] = {}
-tiles[4] = {}
-tiles[5] = {}
-tiles[6] = {}
-tiles[7] = {}
-tiles[8] = {}
-tiles[9] = {}
-tiles[10] = {}
-tiles[12] = {}
-tiles[12] = {}
-tiles[13] = {}
-tiles[14] = {}
-tiles[15] = {}
-tiles[16] = {}
-tiles[17] = {}
-tiles[18] = {}
-tiles[19] = {}
-tiles[20] = {}
-tiles[21] = {}
-tiles[22] = {}
-tiles[23] = {}
-tiles[24] = {}
-tiles[25] = {}
+tiles[1] = {x=0,y=0,type="zone",contains="empty",width=tsx,height=tsy,z_pass=tsz}
+tiles[2] = {x=100,y=0,type="zone",contains="empty",width=tsx,height=tsy,z_pass=tsz}
+tiles[3] = {x=200,y=0,type="zone",contains="empty",width=tsx,height=tsy,z_pass=tsz}
+tiles[4] = {x=300,y=0,type="zone",contains="empty",width=tsx,height=tsy,z_pass=tsz}
+tiles[5] = {x=400,y=0,type="zone",contains="empty",width=tsx,height=tsy,z_pass=tsz}
+
+tiles[6] = {x=0,y=100,type="zone",contains="empty",width=tsx,height=tsy,z_pass=tsz}
+tiles[7] = {x=100,y=100,type="zone",contains="empty",width=tsx,height=tsy,z_pass=tsz}
+tiles[8] = {x=200,y=100,type="zone",contains="empty",width=tsx,height=tsy,z_pass=tsz}
+tiles[9] = {x=300,y=100,type="zone",contains="empty",width=tsx,height=tsy,z_pass=tsz}
+tiles[10] = {x=400,y=100,type="zone",contains="empty",width=tsx,height=tsy,z_pass=tsz}
+
+tiles[12] = {x=0,y=200,type="zone",contains="empty",width=tsx,height=tsy,z_pass=tsz}
+tiles[12] = {x=100,y=200,type="zone",contains="empty",width=tsx,height=tsy,z_pass=tsz}
+tiles[13] = {x=200,y=200,type="zone",contains="empty",width=tsx,height=tsy,z_pass=tsz}
+tiles[14] = {x=300,y=200,type="zone",contains="empty",width=tsx,height=tsy,z_pass=tsz}
+tiles[15] = {x=400,y=200,type="zone",contains="empty",width=tsx,height=tsy,z_pass=tsz}
+
+tiles[16] = {x=0,y=300,type="zone",contains="empty",width=tsx,height=tsy,z_pass=tsz}
+tiles[17] = {x=100,y=300,type="zone",contains="empty",width=tsx,height=tsy,z_pass=tsz}
+tiles[18] = {x=200,y=300,type="zone",contains="empty",width=tsx,height=tsy,z_pass=tsz}
+tiles[19] = {x=300,y=300,type="zone",contains="empty",width=tsx,height=tsy,z_pass=tsz}
+tiles[20] = {x=400,y=300,type="zone",contains="empty",width=tsx,height=tsy,z_pass=tsz}
+
+tiles[21] = {x=0,y=400,type="zone",contains="empty",width=tsx,height=tsy,z_pass=tsz}
+tiles[22] = {x=100,y=400,type="zone",contains="empty",width=tsx,height=tsy,z_pass=tsz}
+tiles[23] = {x=200,y=400,type="zone",contains="empty",width=tsx,height=tsy,z_pass=tsz}
+tiles[24] = {x=300,y=400,type="zone",contains="empty",width=tsx,height=tsy,z_pass=tsz}
+tiles[25] = {x=400,y=400,type="zone",contains="empty",width=tsx,height=tsy,z_pass=tsz}
+
+--rand funcs
+function world.generate_rand_tile_start(type,contains,z_pass)
+  local new_tile
 
 
+
+  return new_tile
+end
 
 function world.update(dt)
+  --check weather, rand weather if timer reaches zero
 
+  --update fires, floods, disasters, etc
+
+  --update world time
+
+  --upate ai
+
+  --update crops, plants
+
+  --check for garbage
 end
 
 function world.draw()
+  --[[
+    order of drawing:
 
+  ]]
 end
 
 return world
