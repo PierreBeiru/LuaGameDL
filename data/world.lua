08ac22f0931099805027af0a31d8645fb4753eff
Jon Vail <jon.vail@yahoo.com>
2021-01-26 00:52:58 -0500

    Create world.lua
    N1

---
+++ data/world.lua
@@ -0,0 +1,46 @@
+local world = {}
+
+local tile_count = 25
+local tile_size_x, tile_size_y = 100, 100 --units
+local tile_size_z = 0
+
+
+
+local tiles = {}
+tiles[1] = {}
+tiles[2] = {}
+tiles[3] = {}
+tiles[4] = {}
+tiles[5] = {}
+tiles[6] = {}
+tiles[7] = {}
+tiles[8] = {}
+tiles[9] = {}
+tiles[10] = {}
+tiles[12] = {}
+tiles[12] = {}
+tiles[13] = {}
+tiles[14] = {}
+tiles[15] = {}
+tiles[16] = {}
+tiles[17] = {}
+tiles[18] = {}
+tiles[19] = {}
+tiles[20] = {}
+tiles[21] = {}
+tiles[22] = {}
+tiles[23] = {}
+tiles[24] = {}
+tiles[25] = {}
+
+
+
+function world.update(dt)
+
+end
+
+function world.draw()
+
+end
+
+return world
